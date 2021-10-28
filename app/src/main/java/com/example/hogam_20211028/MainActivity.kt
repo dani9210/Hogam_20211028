f6d8498a8f6687c7ac7b4311797492b07cde78fb
김다은 <softeun92@naver.com>
2021-10-28 15:06:08 +0900

    1028

--- app/src/main/java/com/example/hogam_20211028/MainActivity.kt
+++ app/src/main/java/com/example/hogam_20211028/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.hogam_20211028
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.hogam_20211028.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.loginBtn.setOnClickListener {
+
+
+
+            val inputEmail = binding.emailEdt.text.toString()
+            val inputPw = binding.passwordEdt.text.toString()
+
+            Log.d("입력이메일",inputEmail)
+            Log.d("입력비밀번호",inputPw)
+
+
+
+        }
+
     }
 }
